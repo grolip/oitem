3b9f9b59460d7325dbed8f048b4cf97b3097e7d9
grolip <grolip@protonmail.com>
2020-11-09 21:45:34 +0100

    Add tests for GetItem and GetChildItem

---
+++ tests/test_get_child_item.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+from pathlib import Path
+
+from oitem import GetChildItem
+
+HOME = Path(__file__).parent.joinpath("home")
+
+def test_wildcard():
+    target = str(HOME.joinpath('*'))
+    item_col = GetChildItem([target])
+    result = [ item for item in item_col.collect() ]
+
+    assert len(result) == 11
+    assert HOME.joinpath('other/other2') in result
+    assert HOME.joinpath('other') not in result
+    assert HOME.joinpath('file1.txt') not in result
+
+def test_recurse():
+    target = str(HOME)
+    item_col = GetChildItem([target], recurse = True)
+    result = [ item for item in item_col.collect() ]
+
+    assert len(result) == 27
+    assert HOME.joinpath('other') in result
+    assert HOME.joinpath('other/other2/ofile1.txt') in result
+    assert HOME not in result
+
+def test_recurse_with_depth():
+    target = str(HOME)
+    item_col = GetChildItem([target], recurse = True, depth = 2)
+    result = [ item for item in item_col.collect() ]
+
+    assert len(result) == 27
+    assert HOME.joinpath('other/other2') in result
+    assert HOME.joinpath('other/other2/ofile1.txt') in result
+    assert HOME not in result
+
+def test_exclude_md():
+    target = str(HOME)
+    item_col = GetChildItem([target], exclude = ["*.md"])
+    result = [ item for item in item_col.collect() ]
+
+    assert len(result) == 6
+    assert HOME.joinpath('other') in result
+    assert HOME.joinpath('file1.md') not in result
+
+def test_include_txt():
+    target = str(HOME)
+    item_col = GetChildItem([target], include = ["*.txt"])
+    result = [ item for item in item_col.collect() ]
+
+    assert len(result) == 5
+    assert HOME.joinpath('file3.txt') in result
+    assert HOME.joinpath('other') not in result
+    assert HOME.joinpath('file1.md') not in result
+
+def test_ignore_dir():
+    target = str(HOME)
+    item_col = GetChildItem([target], ignore_dir = True)
+    result = [ item for item in item_col.collect() ]
+
+    assert len(result) == 10
+    assert HOME.joinpath('file3.txt') in result
+    assert HOME.joinpath('file1.md') in result
+    assert HOME.joinpath('other') not in result
+
+def test_ignore_file():
+    target = str(HOME)
+    item_col = GetChildItem([target], ignore_file = True)
+    result = [ item for item in item_col.collect() ]
+
+    assert len(result) == 1
+    assert HOME.joinpath('other') in result
+    assert HOME.joinpath('file3.txt') not in result
+    assert HOME.joinpath('file1.md') not in result
---
+++ tests/test_get_item.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+from pathlib import Path
+
+from oitem import GetItem
+
+HOME = Path(__file__).parent.joinpath("home")
+
+def test_wildcard():
+    target = str(HOME.joinpath('*'))
+    item_col = GetItem([target])
+    result = [ item for item in item_col.collect() ]
+
+    assert len(result) == 11
+    assert HOME.joinpath('file1.txt') in result
+    assert HOME.joinpath('other') in result
+
+def test_recurse():
+    target = str(HOME)
+    item_col = GetItem([target], recurse = True)
+    result = [ item for item in item_col.collect() ]
+
+    assert len(result) == 28
+    assert HOME in result
+    assert HOME.joinpath('other') in result
+    assert HOME.joinpath('other/other2/ofile1.txt') in result
+
+def test_recurse_with_depth():
+    target = str(HOME)
+    item_col = GetItem([target], recurse = True, depth = 2)
+    result = [ item for item in item_col.collect() ]
+
+    assert len(result) == 23
+    assert HOME.joinpath('other/other2') in result
+    assert HOME.joinpath('other/other2/ofile1.txt') not in result
+
+def test_exclude_md():
+    target = str(HOME.joinpath('*'))
+    item_col = GetItem([target], exclude = ["*.md"])
+    result = [ item for item in item_col.collect() ]
+
+    assert len(result) == 6
+    assert HOME.joinpath('other') in result
+    assert HOME.joinpath('file1.md') not in result
+
+def test_include_txt():
+    target = str(HOME.joinpath('*'))
+    item_col = GetItem([target], include = ["*.txt"])
+    result = [ item for item in item_col.collect() ]
+
+    assert len(result) == 5
+    assert HOME.joinpath('file3.txt') in result
+    assert HOME.joinpath('other') not in result
+    assert HOME.joinpath('file1.md') not in result
+
+def test_ignore_dir():
+    target = str(HOME.joinpath('*'))
+    item_col = GetItem([target], ignore_dir = True)
+    result = [ item for item in item_col.collect() ]
+
+    assert len(result) == 10
+    assert HOME.joinpath('file3.txt') in result
+    assert HOME.joinpath('file1.md') in result
+    assert HOME.joinpath('other') not in result
+
+def test_ignore_file():
+    target = str(HOME.joinpath('*'))
+    item_col = GetItem([target], ignore_file = True)
+    result = [ item for item in item_col.collect() ]
+
+    assert len(result) == 1
+    assert HOME.joinpath('other') in result
+    assert HOME.joinpath('file3.txt') not in result
+    assert HOME.joinpath('file1.md') not in result
