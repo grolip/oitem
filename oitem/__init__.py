0204f47311728b691355132d63b00f75143cada1
grolip <grolip@protonmail.com>
2020-11-10 11:41:35 +0100

    Add ConvertFromJson class

--- oitem/__init__.py
+++ oitem/__init__.py
@@ -1,2 +1,3 @@
 from .get_item import ItemCollector, GetItem
 from .get_child_item import GetChildItem
+from .convert_from_json import ConvertFromJson
---
+++ oitem/convert_from_json.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import json
+from typing import Any, Dict, List, Tuple
+
+from .get_item import AnyPath, GetItem
+
+class ConvertFromJson(GetItem):
+    def __init__(self, 
+                path: List[str], 
+                encoding: str = "utf8",
+                include: List[str] = ["*.json"],
+                *args, 
+                **kwargs):
+        super().__init__(path, include = include, *args, **kwargs)
+        self.encoding = encoding
+        self.ignore_dir = True
+
+    def collect(self) -> Tuple[AnyPath, Dict[str, Any]]:
+        for path in super().collect():
+            try:
+                data = json.loads(path.read_text(encoding = self.encoding))
+                yield (path, data)
+            except (UnicodeDecodeError, json.JSONDecodeError):
+                pass
